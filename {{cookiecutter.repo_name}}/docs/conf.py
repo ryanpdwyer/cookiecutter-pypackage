03bf4b0fc271c91fbe99e7a31e41561006a20233
Ryan Dwyer <ryanpdwyer@gmail.com>
2015-01-15 22:47:33 -0500

    Added a comment about Mocks

--- {{cookiecutter.repo_name}}/docs/conf.py
+++ {{cookiecutter.repo_name}}/docs/conf.py
@@ -26,7 +26,8 @@ if on_rtd:
         def __getattr__(cls, name):
             return Mock()
 
-    MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'pandas']
+    # Add any other difficult to install modules here
+    MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot']
     sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # If extensions (or modules to document with autodoc) are in another directory,
