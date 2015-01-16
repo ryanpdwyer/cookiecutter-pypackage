56d775bca0bbcc349185e5fd77c9929b3e1b39a2
Ryan Dwyer <ryanpdwyer@gmail.com>
2015-01-16 02:37:06 -0500

    Removed pkg_resources boilerplate

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/__init__.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/__init__.py
@@ -4,17 +4,8 @@
 {{ cookiecutter.repo_name }}
 ============================
 """
-import pkg_resources
 
-# Get __version__ from setup.py version number
-# See https://github.com/hgrecco/pint/blob/master/pint/__init__.py
-try:
-    __version__ = pkg_resources.get_distribution('pint').version
-except:
-    # we seem to have a local copy installed without setuptools
-    # so the reported version will be unknown
-    __version__ = "unknown"
-
+# Versioneer versioning
 from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
